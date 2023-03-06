b5282bc11101d698c34f13009b20691e5d5226a8
Nnamdi Okeke <onnamdialex@gmail.com>
2023-03-06 21:30:03 +0100

    :construction: nodeapi work in progress ...

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello there!");
-});
+app.get("/");
 
 const port = 8080;
 app.listen(port, () => {
