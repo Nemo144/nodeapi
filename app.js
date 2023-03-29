77707ed5c0478de48e6cd674cee0414d7f01c76b
Nnamdi Okeke <onnamdialex@gmail.com>
2023-03-29 19:42:26 +0100

    :sparkles: introduce the concept of middleware usage in nodejs

--- app.js
+++ app.js
@@ -2,8 +2,21 @@
 const express = require("express");
 const app = express();
 
+//importing the middleware npm
+const morgan = require("morgan");
+
 //importing routes for use
 const { getPosts } = require("./routes/post");
+
+//to create a sample of middleware
+const myOwnMiddleware = (req, res, next) => {
+  console.log("middleware applied");
+  next();
+};
+
+//using the middleware
+app.use(morgan("dev"));
+app.use(myOwnMiddleware);
 
 app.get("/", getPosts);
 
